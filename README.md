147e8ab8d707dca35b0be94871de0731df006d10
h <h@hs-macbook-pro.myfiosgateway.com>
2021-08-06 22:04:03 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+#### run
+```
+mvn spring-boot:run
+bash run.sh
+```
+
+#### run maven
+```
+mvn clean
+mvn compile
+java -jar target/gs-maven-0.1.0.jar
+```
+#### reference
+```
+https://spring.io/guides/gs/scheduling-tasks/
+https://www.youtube.com/watch?v=PhOkHUcRo3M
+```
---
+++ src/main/java/com/example/schedulingtasks/ScheduledTasks.java
@@ -0,0 +1,25 @@
+package com.example.schedulingtasks;
+import java.util.Date;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@Component
+
+public class ScheduledTasks {
+	@Scheduled(fixedRate = 5000)
+	public void reportCurrentTime() {
+		System.out.println("\n******************************");
+		System.out.println(new Date());
+		System.out.println("******************************");
+	}
+
+	@RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!!!";
+    }
+
+}
